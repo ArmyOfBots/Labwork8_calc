5e56ca26343067e202de829453312cc95bbc0720
Arion <andrey.potrukh@gmail.com>
2022-05-26 22:06:45 +0300

    Commit

--- Labwork8_calc/MainWindow.xaml.cs
+++ Labwork8_calc/MainWindow.xaml.cs
@@ -24,5 +24,143 @@ namespace Labwork8_calc
         {
             InitializeComponent();
         }
+
+        int FirstnumberInt;
+        int SecondnumberInt;
+        int resultInt;
+        public int CurrentEditField = 0;
+        string mathfunc;
+
+        private void Num1_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(1);
+        }
+
+        private void Num2_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(2);
+        }
+
+        private void Num3_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(3);
+        }
+
+        private void Num4_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(4);
+        }
+
+        private void Num5_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(5);
+        }
+
+        private void Num6_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(6);
+        }
+
+        private void Num7_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(7);
+        }
+
+        private void Num8_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(8);
+        }
+        private void Num9_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(9);
+        }
+
+        private void Num0_Click(object sender, RoutedEventArgs e)
+        {
+            Adder(0);
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentEditField = 0;
+            Firstnumber.Content = "";
+            Secondnumber.Content = "";
+            ResultNumber.Content = "";
+            mathfuncmonitor.Content = "";
+        }
+
+        private void subtract_Click(object sender, RoutedEventArgs e)
+        {
+            mathfunc = "subtraction";
+            CurrentEditField = 1;
+            mathfuncmonitor.Content = "-";
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            mathfunc = "addition";
+            CurrentEditField = 1;
+            mathfuncmonitor.Content = "+";
+        }
+
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+            mathfunc = "divide";
+            CurrentEditField = 1;
+            mathfuncmonitor.Content = "/";
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            mathfunc = "multiplication";
+            CurrentEditField = 1;
+            mathfuncmonitor.Content = "*";
+        }
+
+        private void confirm_Click(object sender, RoutedEventArgs e)
+        {
+            FirstnumberInt = Convert.ToInt32(Firstnumber.Content);
+            SecondnumberInt = Convert.ToInt32(Secondnumber.Content);
+            switch (mathfunc)
+            {
+                case "addition":
+                    resultInt = FirstnumberInt + SecondnumberInt;
+                    ResultNumber.Content = ""+ resultInt;
+                    break;
+                case "subtraction":
+                    resultInt = FirstnumberInt - SecondnumberInt;
+                    ResultNumber.Content = "" + resultInt;
+                    break;
+                case "multiplication":
+                    resultInt = FirstnumberInt * SecondnumberInt;
+                    ResultNumber.Content = "" + resultInt;
+                    break;
+                case "divide":
+                    if (SecondnumberInt == 0)
+                    {
+                        MessageBox.Show("Can't divide by 0 yet");
+                    }
+                    else
+                    {
+                        resultInt = FirstnumberInt / SecondnumberInt;
+                        ResultNumber.Content = "" + resultInt;
+                    }
+                    break;
+            }
+        }
+
+        private void Adder(int number)
+        {
+            switch (CurrentEditField)
+            {
+                case 0:
+                    Firstnumber.Content += "" + number;
+                    break;
+                case 1:
+                    Secondnumber.Content += ""+number;
+                    break;
+            }
+        }
     }
+
 }
